f35212e84e150048eab7ea69f870841773a91c8d
Alexander Gusman <alex@goooseman.ru>
2020-10-09 18:59:03 +0300

    chore(storybooks): add storybook for homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { T } from "react-targem";
-import { Text } from "react-native-paper";
+import { Paragraph } from "react-native-paper";
 
 const HomePage: React.FC<HomePageProps> = () => {
   return (
-    <Text>
+    <Paragraph>
       <T message="Open up App.tsx to start working on your app!" />
-    </Text>
+    </Paragraph>
   );
 };
 interface HomePageProps {}
