b7713f7314ad4f378920ecf5d1c00837f7bb0008
Alexander Gusman <alex@goooseman.ru>
2020-10-09 18:17:44 +0300

    chore(storybook): add a locale global

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
+import React from "react";
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import "@storybook/addon-console";
+import TargemProvider from "react-targem";
+import translations from "src/i18n/translations.json";
 
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/controls#show-full-documentation-for-each-property
@@ -8,3 +11,27 @@ export const parameters = {
     viewports: INITIAL_VIEWPORTS,
   },
 };
+
+export const globalTypes = {
+  locale: {
+    name: "Locale",
+    description: "Internationalization locale",
+    defaultValue: "en-GB",
+    toolbar: {
+      icon: "circlehollow",
+      items: [
+        { value: "en-US", right: "🇺🇸", title: "English (US)" },
+        { value: "en-GB", right: "🇬🇧", title: "English (GB)" },
+        { value: "ru", right: "🇷🇺", title: "Русский" },
+      ],
+    },
+  },
+};
+
+export const decorators = [
+  (Story, { globals }) => (
+    <TargemProvider locale={globals.locale} translations={translations}>
+      <Story />
+    </TargemProvider>
+  ),
+];
