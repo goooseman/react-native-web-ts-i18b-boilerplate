545db5cd5277529f394785943b20a481eedf3707
Alexander Gusman <alex@goooseman.ru>
2020-10-09 18:55:55 +0300

    chore(app layout): delete text, that should be on page

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
-import { T, TargemProvider } from "react-targem";
+import { View } from "react-native";
+import { TargemProvider } from "react-targem";
 import styles from "./AppLayout.styles";
 import translations from "src/i18n/translations.json";
 import { Provider as PaperProvider } from "react-native-paper";
@@ -24,9 +24,6 @@ const AppLayoutInternal: React.FC<AppLayoutInternalProps> = ({
         <TargemProvider locale={locale} translations={translations}>
           <View style={styles.container}>
             <LocalSelector />
-            <Text>
-              <T message="Open up App.tsx to start working on your app!" />
-            </Text>
             {children}
             <StatusBar style="auto" />
           </View>
