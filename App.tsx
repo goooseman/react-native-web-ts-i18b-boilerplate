63d96e1d43533901320a718e6574e9abe060c4cd
Alexander Gusman <alex@goooseman.ru>
2020-10-09 15:42:09 +0300

    chore(i18n): install and configure react-targem + gettext-utils

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { T } from "react-targem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        <T message="Open up App.tsx to start working on your app!" />
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
